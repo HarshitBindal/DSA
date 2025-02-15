d5d953a941f2674d4eeb87c12b370c47e6476bd3
Harshit Bindal <142240283+HarshitBindal@users.noreply.github.com>
2025-02-16 01:24:48 +0530

    Time: 1 ms (100.00%) | Memory: 44.1 MB (100.00%) - LeetSync

--- 3723-sum-of-good-numbers/sum-of-good-numbers.java
+++ 3723-sum-of-good-numbers/sum-of-good-numbers.java
@@ -12,15 +12,6 @@ class Solution {
                 if(nums[i]>nums[i-k] && nums[i]>nums[i+k]) sum += nums[i];
             }
         }
-        // for(int i=0; i<k; i++){
-        //     if(nums[i]>nums[i+k]) sum += nums[i];
-        // }
-        // for(int i=k; i<nums.length-k; i++){
-        //     if(nums[i]>nums[i+k] && nums[i]>nums[i-k]) sum += nums[i];
-        // }
-        // for(int i=k+1; i<nums.length; i++){
-        //     if(nums[i]>nums[i-k]) sum += nums[i];
-        // }
         return sum;
     }
 }
