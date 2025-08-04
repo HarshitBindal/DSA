71c5cbb7c1e77cbd302731e9392c1a8f9ecdb005
Harshit Bindal <142240283+HarshitBindal@users.noreply.github.com>
2025-08-05 01:26:48 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (27.04%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.java
+++ 216-combination-sum-iii/combination-sum-iii.java
@@ -1,16 +1,16 @@
 class Solution {
+    public List<List<Integer>> res = new ArrayList<>();
+    public List<Integer> set = new ArrayList<>();
     public List<List<Integer>> combinationSum3(int k, int n) {
-        List<List<Integer>> res = new ArrayList<>();
-        List<Integer> set = new ArrayList<>();
         int[] nums = new int[9];
         for(int i=0; i<9; i++){
             nums[i] = i+1;
         }
-        helper(res, set, nums, 0, 0, k, n, 0);
+        helper(nums, 0, 0, k, n, 0);
         return res;
     }
 
-    private void helper(List<List<Integer>> res, List<Integer> set, int[] nums,
+    private void helper(int[] nums,
     int sum, int i, int k, int n, int size){
 
         if(sum>n) return;
@@ -20,7 +20,7 @@ class Solution {
         }
         for(int j=i; j<9; j++){
             set.add(nums[j]);
-            helper(res, set, nums, sum+nums[j], j+1, k, n, size+1);
+            helper(nums, sum+nums[j], j+1, k, n, size+1);
             set.remove(set.size()-1);
         }
     }
