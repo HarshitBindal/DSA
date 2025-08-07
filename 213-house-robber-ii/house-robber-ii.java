8f26e0fad5e40166b7e0c90ee4a7b0d746cb4f94
Harshit Bindal <142240283+HarshitBindal@users.noreply.github.com>
2025-08-07 18:23:14 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (49.47%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        // int[] temp1 = new int[n];
+        // int[] temp2 = new int[n];
+        // for(int i=0; i<n; i++){
+        //     if(i != 0) temp1[i]=nums[i];
+        //     if(i != n-1) temp2[i]=nums[i];
+        // }
+        if(n==1) return nums[0];
+        int max1 = func(Arrays.copyOfRange(nums, 1, n));
+        int max2 = func(Arrays.copyOfRange(nums, 0, n-1));
+        return Math.max(max1, max2);
+    }
+    private int func(int[] nums){
+        int n = nums.length;
+        int prev = nums[0];
+        int prev2 = 0;
+        for(int i=1; i<n; i++){
+            int take = nums[i] + prev2;
+            int notTake = prev;
+            int curr = Math.max(take, notTake);
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
+    }
+}
