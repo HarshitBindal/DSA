254ee5ce19d235f83d5a634a9f3e692be13a2bca
Harshit Bindal <142240283+HarshitBindal@users.noreply.github.com>
2025-08-21 20:30:02 +0530

    Time: 5 ms (83.81%) | Memory: 43.5 MB (46.19%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,13 +1,26 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        return helper(0, target, nums);
+        int totsum = 0;
+        for(int num: nums) totsum += num;
+        if(totsum-target<0 || (totsum-target)%2 != 0) return 0;
+        return helper((totsum-target)/2, nums);
     }
-    private int helper(int ind, int tar, int[] nums){
-        if(ind==nums.length){
-            return tar==0? 1: 0;
+    private int helper(int tar, int[] nums){
+        int n = nums.length;
+        int[][] dp = new int[n][tar+1];
+        if(nums[0]==0) dp[0][0] = 2;
+        else dp[0][0] = 1;
+        if(nums[0]!=0 && nums[0]<=tar){
+            dp[0][nums[0]] = 1;
         }
-        int plus = helper(ind+1, tar-nums[ind], nums);
-        int minus = helper(ind+1, tar+nums[ind], nums);
-        return plus+minus;
+        for(int i=1; i<n; i++){
+            for(int sum=0; sum<=tar; sum++){
+                int notTake = dp[i-1][sum];
+                int take = 0;
+                if(nums[i]<=sum) take = dp[i-1][sum-nums[i]];
+                dp[i][sum] = notTake + take;
+            }
+        }
+        return dp[n-1][tar];
     }
 }
