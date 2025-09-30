55b9b00b1ce21da9ee879bff4c099fb6a1ba1e02
Harshit Bindal <142240283+HarshitBindal@users.noreply.github.com>
2025-09-30 16:35:38 +0530

    Time: 1 ms (82.60%) | Memory: 41.9 MB (45.54%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -16,18 +16,4 @@ class Solution {
         }
         return dp[0][1];
     }
-    private int helper(int ind, int buy, int[] prices, int[][] dp){
-        if(ind >= prices.length) return 0;
-        if(dp[ind][buy] != -1){
-            return dp[ind][buy];
-        }
-        if(buy==1){
-            return Math.max(helper(ind+1, 0, prices, dp)-prices[ind],
-            helper(ind+1, 1, prices, dp));
-        }
-        else {
-            return Math.max(helper(ind+2, 1, prices, dp)+prices[ind],
-            helper(ind+1, 0, prices, dp));
-        }
-    }
 }
