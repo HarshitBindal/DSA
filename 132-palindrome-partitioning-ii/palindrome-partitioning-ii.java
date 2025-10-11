0a9a6a3038d8e0503feace182560dbee4cd8a0de
Harshit Bindal <142240283+HarshitBindal@users.noreply.github.com>
2025-10-11 15:01:39 +0530

    Time: 2303 ms (41.22%) | Memory: 41.4 MB (91.76%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
@@ -1,20 +1,19 @@
 class Solution {
     public int minCut(String s) {
-        int[] dp = new int[s.length()];
-        Arrays.fill(dp, -1);
-        return func(0, s, dp)-1;
-    }
-    private int func(int ind, String s, int[] dp){
-        if(ind==s.length()) return 0;
-        if(dp[ind] != -1) return dp[ind];
-        int minCost = Integer.MAX_VALUE;
-        for(int j=ind; j<s.length(); j++){
-            if(isPalindrome(ind, j, s)){
-                int cost = 1+ func(j+1, s, dp);
-                minCost = Math.min(minCost, cost);
+        int n = s.length();
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, 0);
+        for(int i=n-1; i>=0; i--){
+            int minCost = Integer.MAX_VALUE;
+            for(int j=i; j<n; j++){
+                if(isPalindrome(i, j, s)){
+                    int cost = 1+ dp[j+1];
+                    minCost = Math.min(minCost, cost);
+                }
             }
+            dp[i] = minCost;
         }
-        return dp[ind] = minCost;
+        return dp[0]-1;
     }
     private boolean isPalindrome(int i, int j, String temp){
         while(i<j){
