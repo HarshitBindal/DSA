6985702a176dd256d68a96f4c6d9af1690b30d27
Harshit Bindal <142240283+HarshitBindal@users.noreply.github.com>
2025-12-22 11:54:45 +0530

    Time: 170 ms (50.00%) | Memory: 186.3 MB (0.00%) - LeetSync

---
+++ 4162-minimum-number-of-operations-to-have-distinct-elements/minimum-number-of-operations-to-have-distinct-elements.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        HashMap<Integer, Integer> freq = new HashMap<>();
+        int total = 0;
+        for(int num: nums){
+            freq.put(num, freq.getOrDefault(num,0)+1);
+            total++;
+        }
+        int j=0;
+        int count = 0;
+        while(j<nums.length && freq.size()<total){
+            boolean del = false;
+            for(int i=j; i<nums.length && i<j+3; i++){
+                freq.put(nums[i], freq.get(nums[i])-1);
+                if(freq.get(nums[i])==0) freq.remove(nums[i]);
+                total--;
+                del = true;
+            }
+            if(del) count++;
+            j+=3;
+        }
+        return count;
+    }
+}
