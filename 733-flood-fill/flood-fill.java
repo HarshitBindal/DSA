5788d098c50e1fcd63da85acae522f53aa601639
Harshit Bindal <142240283+HarshitBindal@users.noreply.github.com>
2025-12-25 19:21:27 +0530

    Time: 1 ms (44.35%) | Memory: 46.9 MB (18.91%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,18 +1,28 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        if(image[sr][sc] == color){
-            return image;
-        }
-        fill(image, sr, sc, color, image[sr][sc]);
-        return image;
-    }
-    private void fill(int[][] image, int sr, int sc, int color, int curr){
-        if(sr<0 || sr>=image.length || sc<0 || sc>=image[0].length) return;
-        if(curr != image[sr][sc]) return;
+        int initialColor = image[sr][sc];
+        if(initialColor==color) return image;
+        int n = image.length;
+        int m = image[0].length;
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{sr, sc});
         image[sr][sc] = color;
-        fill(image, sr-1, sc, color, curr);
-        fill(image, sr, sc+1, color, curr);
-        fill(image, sr+1, sc, color, curr);
-        fill(image, sr, sc-1, color, curr);
+        int[] drow = {-1, 0, 1, 0};
+        int[] dcol = {0, 1, 0, -1};
+        while(!q.isEmpty()){
+            int r = q.peek()[0];
+            int c = q.peek()[1];
+            q.poll();
+            for(int i=0; i<4; i++){
+                int nrow = drow[i] + r;
+                int ncol = dcol[i] + c;
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && 
+                image[nrow][ncol]==initialColor){
+                    image[nrow][ncol] = color;
+                    q.offer(new int[]{nrow, ncol});
+                }
+            }
+        }
+        return image;
     }
 }
