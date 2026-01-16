73ff4fde9ca3426ed45194a498847b27a0923e11
Harshit Bindal <142240283+HarshitBindal@users.noreply.github.com>
2026-01-16 14:23:23 +0530

    Time: 84 ms (29.79%) | Memory: 72.9 MB (26.58%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,37 +1,19 @@
 class Solution {
     public boolean canPartition(int[] nums) {
+        int n = nums.length;
         int sum = 0;
-        for(int num: nums){
-            sum += num;
-        }
-        if(sum%2 != 0) return false;
-        int target = sum/2;
-        boolean[][] dp = new boolean[nums.length][target+1];
-        for(int i=0; i<nums.length; i++){
-            dp[i][0] = true;
-        }
-        if(nums[0]<=target) dp[0][nums[0]] = true;
-        for(int i=1; i<dp.length; i++){
-            for(int t=1; t<=target; t++){
-                boolean notTake = dp[i-1][t];
-                boolean take = false;
-                if(nums[i]<=t){
-                    take = dp[i-1][t-nums[i]];
-                }
-                dp[i][t] = take || notTake;
-            }
-        }
-        return dp[nums.length-1][target];
+        for(int num: nums) sum+=num;
+        if(sum%2!=0) return false;
+        Boolean[][] dp = new Boolean[n][sum/2 + 1];
+        return helper(n-1, sum/2, nums, dp);
     }
-    // private boolean helper(int ind, int target, int[] nums, int[][] dp){
-    //     if(target==0) return true;
-    //     if(ind==0) return nums[0]==target;
-    //     if(target<0) return false;
-    //     if(dp[ind][target] != -1) dp[ind][target] = 1;
-
-    //     boolean take = helper(ind-1, target-nums[ind], nums, dp);
-    //     boolean notTake = helper(ind-1, target, nums, dp);
-    //     dp[ind][target] = (take || notTake) ? 1:0;
-    //     return dp[ind][target] == 1;
-    // }
+    private boolean helper(int ind, int target, int[] nums, Boolean[][] dp){
+        if(target==0) return true;
+        if(ind<0 || target<0) return false;
+        if(ind==0) return nums[ind]==target;
+        if(dp[ind][target]!=null) return dp[ind][target];
+        boolean take = helper(ind-1, target-nums[ind], nums, dp);
+        boolean notTake = helper(ind-1, target, nums, dp);
+        return dp[ind][target]=take||notTake;
+    }
 }
