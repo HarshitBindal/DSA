b8d99097c6f06c0df82ae4ecf6b0696186e3108e
Harshit Bindal <142240283+HarshitBindal@users.noreply.github.com>
2026-01-17 23:49:35 +0530

    Time: 5 ms (65.78%) | Memory: 47.3 MB (40.41%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -2,26 +2,31 @@ class Solution {
     public int minDistance(String word1, String word2) {
         int n = word1.length();
         int m = word2.length();
-        int[][] dp = new int[n][m];
-        for(int[] row: dp){
-            Arrays.fill(row, -1);
+        int[][] dp = new int[n+1][m+1];
+        for(int i=0; i<=n; i++){
+            dp[i][0]=i;
         }
-        // for(int i=0; i<=n; i++){
-        //     dp[i][0] = 1;
-        // }
-        // for(int j=0; j<=m; j++){
-        //     dp[0][j] = 1;
-        // }
-        return helper(n-1, m-1, word1, word2, dp);
+        for(int j=0; j<=m; j++){
+            dp[0][j]=j;
+        }
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(word1.charAt(i-1)==word2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else dp[i][j]=Math.min(dp[i-1][j], Math.min(dp[i][j-1], dp[i-1][j-1]))+1;
+            }
+        }
+        return dp[n][m];
     }
     private int helper(int i, int j, String word1, String word2, int[][] dp){
-        if(i<0) return j+1;
         if(j<0) return i+1;
-        if(dp[i][j] != -1) return dp[i][j];
-        if(word1.charAt(i)==word2.charAt(j)){
-            return dp[i][j] = helper(i-1, j-1, word1, word2, dp);
-        }
-        return dp[i][j] = Math.min(1+helper(i,j-1,word1,word2, dp), 
-        Math.min(1+helper(i-1,j,word1, word2,dp), 1+helper(i-1,j-1,word1, word2, dp)));
+        if(i<0) return j+1;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(word1.charAt(i)==word2.charAt(j)) return dp[i][j]=helper(i-1,j-1,word1,word2,dp);
+        int delete = helper(i-1,j,word1,word2,dp)+1;
+        int insert = helper(i,j-1,word1,word2,dp)+1;
+        int replace = helper(i-1,j-1,word1,word2,dp)+1;
+        return dp[i][j] = Math.min(delete, Math.min(insert, replace));
     }
 }
