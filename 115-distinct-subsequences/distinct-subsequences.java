0408523c56a4e60479227d58aabce90e4f85fee3
Harshit Bindal <142240283+HarshitBindal@users.noreply.github.com>
2026-01-17 23:03:43 +0530

    Time: 27 ms (9.34%) | Memory: 54.3 MB (68.61%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,23 +1,18 @@
 class Solution {
     public int numDistinct(String s, String t) {
-        int n = s.length();
-        int m = t.length();
-        int[][] dp = new int[n+1][m+1];
-        for(int i=0; i<=n; i++){
-            dp[i][0] = 1;
+        int[][] dp = new int[s.length()][t.length()];
+        for(int[] curr: dp){
+            Arrays.fill(curr,-1);
         }
-        for(int j=1; j<=m; j++){
-            dp[0][j] = 0;
-        }
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=m; j++){
-                if(s.charAt(i-1) == t.charAt(j-1)){
-                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
-                } else {
-                    dp[i][j] = dp[i-1][j];
-                }
-            }
-        }
-        return dp[n][m];
+        return helper(s.length()-1, t.length()-1, s, t, dp);
+    }
+    private int helper(int i, int j, String s, String t, int[][] dp){
+        if(j<0) return 1;
+        if(i<0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int pick = 0;
+        if(s.charAt(i)==t.charAt(j)) pick = helper(i-1,j-1,s,t,dp);
+        int notpick = helper(i-1,j,s,t,dp);
+        return dp[i][j]=pick+notpick;
     }
 }
