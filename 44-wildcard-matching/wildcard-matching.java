a04746a00a49fdccd92b7bbc0d1ec698c8cf1e5b
Harshit Bindal <142240283+HarshitBindal@users.noreply.github.com>
2026-01-18 00:17:38 +0530

    Time: 14 ms (75.29%) | Memory: 60.6 MB (28.51%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -2,29 +2,25 @@ class Solution {
     public boolean isMatch(String s, String p) {
         int n = p.length();
         int m = s.length();
-        boolean[][] dp = new boolean[n+1][m+1];
-        dp[0][0] = true;
-        for(int j=1; j<=m; j++) dp[0][j] = false;
-        for(int i=1; i<=n; i++){
-            boolean flag = true;
-            for(int x=1; x<=i; x++){
-                if(p.charAt(x-1) != '*'){
-                    flag = false;
-                } 
+        Boolean[][] dp = new Boolean[n][m];
+        return helper(n-1,m-1,s,p,dp);
+    }
+    private boolean helper(int i, int j, String s, String p, Boolean[][] dp){
+        if(i<0 && j<0) return true;
+        if(i<0 && j>=0) return false;
+        if(j<0 && i>=0){
+            for(int k=0; k<=i; k++){
+                if(p.charAt(k)!='*') return false;
             }
-            dp[i][0] = flag;
+            return true;
         }
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=m; j++){
-                if(s.charAt(j-1)==p.charAt(i-1) || p.charAt(i-1)=='?'){
-                    dp[i][j] = dp[i-1][j-1];
-                } 
-                else if(p.charAt(i-1)=='*'){
-                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
-                }
-                else dp[i][j] = false;
-            }
+        if(dp[i][j]!=null) return dp[i][j];
+        if(s.charAt(j)==p.charAt(i) || p.charAt(i)=='?'){
+            return dp[i][j] = helper(i-1,j-1,s,p,dp);
+        } 
+        else if(p.charAt(i)=='*'){
+            return dp[i][j] = helper(i-1,j,s,p,dp) || helper(i,j-1,s,p,dp);
         }
-        return dp[n][m];
+        return dp[i][j] = false;
     }
 }
