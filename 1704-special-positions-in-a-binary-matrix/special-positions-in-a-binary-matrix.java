0f9993e00787d94f8ac782381f56253d1602779a
Harshit Bindal <142240283+HarshitBindal@users.noreply.github.com>
2026-03-05 00:27:24 +0530

    Time: 2 ms (94.84%) | Memory: 47.1 MB (83.45%) - LeetSync

--- 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.java
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.java
@@ -1,38 +1,21 @@
 class Solution {
     public int numSpecial(int[][] mat) {
-        int cnt=0;
         int n = mat.length;
         int m = mat[0].length;
+        int cnt=0;
+        int[] row = new int[n];
+        int[] col = new int[m];
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 if(mat[i][j]==1){
-                    boolean poss = true;
-                    for(int col=0; col<j; col++){
-                        if(mat[i][col]==1){
-                            poss = false;
-                            break;
-                        } 
-                    }
-                    for(int col=j+1; col<m; col++){
-                        if(mat[i][col]==1){
-                            poss = false;
-                            break;
-                        }
-                    }
-                    for(int row=0; row<i; row++){
-                        if(mat[row][j]==1){
-                            poss = false;
-                            break;
-                        }
-                    }
-                    for(int row=i+1; row<n; row++){
-                        if(mat[row][j]==1){
-                            poss = false;
-                            break;
-                        }
-                    }
-                    if(poss) cnt++;
+                    row[i]++;
+                    col[j]++;
                 }
+            }
+        }
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(mat[i][j]==1 && row[i]==1 && col[j]==1) cnt++;
             }
         }
         return cnt;
