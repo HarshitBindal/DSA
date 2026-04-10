45b964df608f3a01615680de424b7508e7e51fb8
Harshit Bindal <142240283+HarshitBindal@users.noreply.github.com>
2026-04-10 15:02:56 +0530

    Time: 2 ms (89.97%) | Memory: 44 MB (86.14%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -2,39 +2,42 @@ class Solution {
     public int orangesRotting(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-        int[][] visit = new int[n][m];
-        Queue<int[]> q = new LinkedList<>();
-        int cntfresh = 0;
+        int fresh = 0;
+        int rotten = 0;
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                visit[i][j] = grid[i][j];
-                if(grid[i][j]==1){
-                    cntfresh++;
-                } else if(grid[i][j]==2) {
-                    q.offer(new int[]{i, j, 0});
+                if(grid[i][j]==1) fresh++;
+                else if(grid[i][j]==2) rotten++;
+            }
+        }
+        // if(rotten==0) return -1;
+        Queue<int[]> q = new LinkedList<>();
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(grid[i][j]==2) {
+                    q.offer(new int[]{i,j,0});
+                    // break;
                 }
             }
         }
-        int[] drow = {-1, 0, 1, 0};
-        int[] dcol = {0, 1, 0, -1};
-        int cnt = 0, tm = 0;
+        int[] drow = {-1,0,1,0};
+        int[] dcol = {0,1,0,-1};
+        int time = 0;
         while(!q.isEmpty()){
-            int r = q.peek()[0];
-            int c = q.peek()[1];
-            int t = q.peek()[2];
-            q.poll();
+            int[] curr = q.poll();
             for(int i=0; i<4; i++){
-                int nrow = r + drow[i];
-                int ncol = c + dcol[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && visit[nrow][ncol]==1 
-                && grid[nrow][ncol]==1){
-                    q.offer(new int[]{nrow, ncol, t+1});
-                    cnt++;
-                    visit[nrow][ncol] = 2;
+                int nrow = curr[0]+drow[i];
+                int ncol = curr[1]+dcol[i];
+                int tm = curr[2];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==1){
+                    q.offer(new int[]{nrow,ncol,tm+1});
+                    grid[nrow][ncol]=2;
+                    fresh--;
+                    time=tm+1;
                 }
             }
-            tm = Math.max(tm, t);
         }
-        return cnt!=cntfresh? -1: tm;
+        if(fresh!=0) return -1;
+        return time;
     }
 }
