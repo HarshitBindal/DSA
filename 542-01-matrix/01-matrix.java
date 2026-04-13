c49b6eff5616f0d890892f57b004607ab96e5741
Harshit Bindal <142240283+HarshitBindal@users.noreply.github.com>
2026-04-13 16:32:03 +0530

    Time: 15 ms (44.10%) | Memory: 49.8 MB (12.42%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -2,35 +2,31 @@ class Solution {
     public int[][] updateMatrix(int[][] mat) {
         int n = mat.length;
         int m = mat[0].length;
+        int[][] res = new int[n][m];
+        boolean[][] visited = new boolean[n][m];
         Queue<int[]> q = new LinkedList<>();
-        boolean[][] seen = new boolean[n][m];
-        int[][] visit = new int[n][m];
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 if(mat[i][j]==0){
                     q.offer(new int[]{i,j,0});
-                    seen[i][j] = true;
+                    visited[i][j] = true;
                 } 
             }
         }
         int[] drow = {-1,0,1,0};
         int[] dcol = {0,1,0,-1};
         while(!q.isEmpty()){
-            int r = q.peek()[0];
-            int c = q.peek()[1];
-            int steps = q.peek()[2];
-            visit[r][c] = steps;
-            q.poll();
+            int[] curr = q.poll();
+            res[curr[0]][curr[1]] = curr[2];
             for(int i=0; i<4; i++){
-                int nrow = r + drow[i];
-                int ncol = c + dcol[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m &&
-                !seen[nrow][ncol] && mat[nrow][ncol]!=0){
-                    seen[nrow][ncol] = true;
-                    q.offer(new int[]{nrow, ncol, steps+1});
+                int nr = drow[i]+curr[0];
+                int nc = dcol[i]+curr[1];
+                if(nr>=0 && nr<n && nc>=0 && nc<m && !visited[nr][nc] && mat[nr][nc]==1){
+                    visited[nr][nc] = true;
+                    q.offer(new int[]{nr,nc,curr[2]+1});
                 }
             }
         }
-        return visit;
+        return res;
     }
 }
