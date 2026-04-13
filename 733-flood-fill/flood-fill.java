9ab4102a08beadd03e65f5a30e928703d2305c8c
Harshit Bindal <142240283+HarshitBindal@users.noreply.github.com>
2026-04-13 14:03:54 +0530

    Time: 1 ms (39.87%) | Memory: 47 MB (30.04%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,25 +1,25 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int initialColor = image[sr][sc];
-        if(initialColor==color) return image;
         int n = image.length;
         int m = image[0].length;
         Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{sr, sc});
-        image[sr][sc] = color;
+        q.offer(new int[]{sr, sc, image[sr][sc]});
+        boolean[][] visited = new boolean[n][m];
         int[] drow = {-1, 0, 1, 0};
         int[] dcol = {0, 1, 0, -1};
+        image[sr][sc]=color;
+        visited[sr][sc]=true;
         while(!q.isEmpty()){
-            int r = q.peek()[0];
-            int c = q.peek()[1];
-            q.poll();
+            int[] curr = q.poll();
+            int prevCol = curr[2];
             for(int i=0; i<4; i++){
-                int nrow = drow[i] + r;
-                int ncol = dcol[i] + c;
+                int nrow = drow[i]+curr[0];
+                int ncol = dcol[i]+curr[1];
                 if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && 
-                image[nrow][ncol]==initialColor){
+                image[nrow][ncol]==prevCol && !visited[nrow][ncol]){
+                    q.offer(new int[]{nrow, ncol, image[nrow][ncol]});
                     image[nrow][ncol] = color;
-                    q.offer(new int[]{nrow, ncol});
+                    visited[nrow][ncol]=true;
                 }
             }
         }
